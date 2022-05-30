492edbaea34ad1b995c619e1b77d963faf9795b0
Emil-Linnebjerg <emillinnebjerg@gmail.com>
2022-05-30 13:59:19 +0200

    Mostly layout

--- frontend/src/Views/Board.tsx
+++ frontend/src/Views/Board.tsx
@@ -1,242 +1,232 @@
-import React, {useCallback, useState, useReducer } from "react";
-import ReactDOM from "react-dom";
+import React, { useCallback, useState, useReducer } from 'react';
+import ReactDOM from 'react-dom';
 import CSS from 'csstype';
-import {Issue, IssueListTemp, getIssues} from '../ViewModels/Board'
-import produce from "immer";
+import { Issue, IssueListTemp, getIssues } from '../ViewModels/Board';
+import produce from 'immer';
 import {
-  DragDropContext,
-  Draggable,
-  DraggableProvided,
-  DraggingStyle,
-  Droppable,
-  DropResult,
-  NotDraggingStyle
-} from "react-beautiful-dnd";
+    DragDropContext,
+    Draggable,
+    DraggableProvided,
+    DraggingStyle,
+    Droppable,
+    DropResult,
+    NotDraggingStyle,
+} from 'react-beautiful-dnd';
 
 const horizontalList: CSS.Properties = {
-  float: 'left',
-  padding: '0.8rem',
-  border: 'dotted',
-  margin: '0.2rem',
-}
+    float: 'left',
+    padding: '0.8rem',
+    border: 'dotted',
+    margin: '0.2rem',
+};
 
 const issueStyle: CSS.Properties = {
-  padding: '0.2rem',
-  border: 'dotted',
-  margin: '0.1rem',
+    padding: '0.2rem',
+    border: 'dotted',
+    margin: '0.1rem',
+};
+
+function fetchIssues(): Issue[] {
+    const testA = data;
+
+    return testA;
 }
 
-function fetchIssues() : Issue[]{
-  const testA = data;
+function fetchIssueLists(): IssueListTemp[] {
+    const testA = {
+        name: 'string',
+        content: fetchIssues(),
+    };
 
-  return (testA);
-}
-
-function fetchIssueLists() : IssueListTemp[]{
-  const testA = {
-    name: "string",
-    content: fetchIssues()
-  };
-
-  return ([testA]);
+    return [testA];
 }
 
 const dragReducer = produce((draft, action) => {
-  switch (action.type) {
-    case "MOVE": {
-      draft[action.from] = draft[action.from] || [];
-      draft[action.to] = draft[action.to] || [];
-      const [removed] = draft[action.from].splice(action.fromIndex, 1);
-      draft[action.to].splice(action.toIndex, 0, removed);
+    switch (action.type) {
+        case 'MOVE': {
+            draft[action.from] = draft[action.from] || [];
+            draft[action.to] = draft[action.to] || [];
+            const [removed] = draft[action.from].splice(action.fromIndex, 1);
+            draft[action.to].splice(action.toIndex, 0, removed);
+            return;
+        }
+        case 'ADDITEM': {
+            return [...draft, action.value];
+        }
     }
-  }
 });
 
-let issueIdIncrement = 5;
+let issueIdIncrement = 6;
 
-function App (){
-  const [state, dispatch] : [any, React.Dispatch<any>] = useReducer(dragReducer, {
-    items: data,
-  }
-  );
+function App() {
+    const [test, setTest] = useState(data);
+    const [state, dispatch]: [any, React.Dispatch<any>] = useReducer(dragReducer, {
+        items: test, //array instead :)
+        items2: data2,
+    });
 
-  const onDragEnd = useCallback((result : any) => {
-    if (result.reason === "DROP") {
-      if (!result.destination) {
-        return;
-      }
-      dispatch({
-        type: "MOVE",
-        from: result.source.droppableId,
-        to: result.destination.droppableId,
-        fromIndex: result.source.index,
-        toIndex: result.destination.index,
-      });
-    }
-  }, []);
-
-  return (
-    <div>
-      <button>Add Issue List</button>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="items" type="PERSON">
-          {(provided, snapshot) => 
-            {    return (
-              <div style={horizontalList}
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-              >
-                {state.items?.map((issue : Issue, index : number) => 
-                  arrangeIssueInList(issue, index)
-                )}
-                {provided.placeholder}
-                <button onClick={
-                  () => {
-                    issueIdIncrement++;
-                    data.push(
-                      {
-                        id: issueIdIncrement.toString(),
-                        content: "This list is buggy"
-                      }
-                    );
-                  }
-                }
-                >Add Issue</button>
-              </div>
-            );}
-          }
-        </Droppable>
-        <Droppable droppableId="items2" type="PERSON">
-          {(provided, snapshot) => 
-            {    return (
-              <div style={horizontalList}
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                
-              >
-                {state.items2?.map((issue : Issue, index : number) => 
-                arrangeIssueInList(issue, index)
-                )}
-                {provided.placeholder}
-                <button onClick={
-                  () => {
-                    issueIdIncrement++;
-                    data.push(
-                      {
-                        id: issueIdIncrement.toString(),
-                        content: "This list is buggy"
-                      }
-                    );
-                  }
-                }
-                >Add Issue</button>
-              </div>
-            );}
-          }
-        </Droppable>
-        <Droppable droppableId="items3" type="PERSON">
-          {(provided, snapshot) => 
-            {    return (
-              <div style={horizontalList}
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                
-              >
-                {state.items3?.map((issue : Issue, index : number) => 
-                arrangeIssueInList(issue, index)
-                )}
-                {provided.placeholder}
-                <button onClick={
-                  () => {
-                    issueIdIncrement++;
-                    data.push(
-                      {
-                        id: issueIdIncrement.toString(),
-                        content: "This list is buggy"
-                      }
-                    );
-                  }
-                }
-                >Add Issue</button>
-              </div>
-            );}
-          }
-        </Droppable>
-      </DragDropContext>
-    </div>
-  );
-
-  function arrangeDragDropForIssueList(provided : any, state: any, mapItem : any){
-    return (
-      <div style={horizontalList}
-        ref={provided.innerRef}
-        {...provided.droppableProps}
-        
-      >
-        {mapItem?.map((person : any, index : any) => 
-        arrangeIssueInList(person, index)
-        )}
-        {provided.placeholder}
-        <button onClick={
-          () => {}
+    const onDragEnd = useCallback((result: any) => {
+        if (result.reason === 'DROP') {
+            if (!result.destination) {
+                return;
+            }
+            dispatch({
+                type: 'MOVE',
+                from: result.source.droppableId,
+                to: result.destination.droppableId,
+                fromIndex: result.source.index,
+                toIndex: result.destination.index,
+            });
         }
-        >Add Issue</button>
-      </div>
+    }, []);
+
+    return (
+        <div>
+            <button>Add Issue List</button>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="items" type="PERSON">
+                    {(provided, snapshot) => {
+                        return (
+                            <div style={horizontalList} ref={provided.innerRef} {...provided.droppableProps}>
+                                {state.items?.map((issue: Issue, index: number) => arrangeIssueInList(issue, index))}
+                                {provided.placeholder}
+                                <button
+                                    onClick={() => {
+                                        issueIdIncrement++;
+                                        setTest(() => [
+                                            ...test,
+                                            {
+                                                id: issueIdIncrement.toString(),
+                                                content: 'This list is buggy',
+                                            },
+                                        ]);
+                                        data.push({
+                                            id: issueIdIncrement.toString(),
+                                            content: 'This list is buggy',
+                                        });
+                                    }}
+                                >
+                                    Add Issue
+                                </button>
+                            </div>
+                        );
+                    }}
+                </Droppable>
+                <Droppable droppableId="items2" type="PERSON">
+                    {(provided, snapshot) => {
+                        return (
+                            <div style={horizontalList} ref={provided.innerRef} {...provided.droppableProps}>
+                                {state.items2?.map((issue: Issue, index: number) => arrangeIssueInList(issue, index))}
+                                {provided.placeholder}
+                                <button
+                                    onClick={() => {
+                                        issueIdIncrement++;
+                                        setTest(() => [
+                                            ...test,
+                                            {
+                                                id: issueIdIncrement.toString(),
+                                                content: 'This list is buggy',
+                                            },
+                                        ]);
+                                        data.push({
+                                            id: issueIdIncrement.toString(),
+                                            content: 'This list is buggy',
+                                        });
+                                    }}
+                                >
+                                    Add Issue
+                                </button>
+                            </div>
+                        );
+                    }}
+                </Droppable>
+                <Droppable droppableId="items3" type="PERSON">
+                    {(provided, snapshot) => {
+                        return (
+                            <div style={horizontalList} ref={provided.innerRef} {...provided.droppableProps}>
+                                {state.items3?.map((issue: Issue, index: number) => arrangeIssueInList(issue, index))}
+                                {provided.placeholder}
+                                <button
+                                    onClick={() => {
+                                        issueIdIncrement++;
+                                        setTest(() => [
+                                            ...test,
+                                            {
+                                                id: issueIdIncrement.toString(),
+                                                content: 'This list is buggy',
+                                            },
+                                        ]);
+                                        data.push({
+                                            id: issueIdIncrement.toString(),
+                                            content: 'This list is buggy',
+                                        });
+                                    }}
+                                >
+                                    Add Issue
+                                </button>
+                            </div>
+                        );
+                    }}
+                </Droppable>
+            </DragDropContext>
+        </div>
     );
-  }
-};
 
-function arrangeIssueInList(issue : Issue, index : number){
-  return (
-    <Draggable
-      key={issue.id}
-      draggableId={issue.id}
-      index={index}
-    >
-      {(provided, snapshot) => 
-        arrangeIssue(provided, issue)
-      }
-    </Draggable>
-  );
+    function arrangeDragDropForIssueList(provided: any, state: any, mapItem: any) {
+        return (
+            <div style={horizontalList} ref={provided.innerRef} {...provided.droppableProps}>
+                {mapItem?.map((person: any, index: any) => arrangeIssueInList(person, index))}
+                {provided.placeholder}
+                <button onClick={() => {}}>Add Issue</button>
+            </div>
+        );
+    }
 }
 
-function arrangeIssue(provided : DraggableProvided, issue : Issue){
-  return (
-    <div
-      ref={provided.innerRef}
-      {...provided.draggableProps}
-      {...provided.dragHandleProps}
-    >
-      <div style={issueStyle}>
-        <span>
-          {issue.content}
-        </span>
-      </div>
-    </div>
-  );
+function arrangeIssueInList(issue: Issue, index: number) {
+    return (
+        <Draggable key={issue.id} draggableId={issue.id} index={index}>
+            {(provided, snapshot) => arrangeIssue(provided, issue)}
+        </Draggable>
+    );
 }
 
-export const data :Issue[] = [
-  {
-    id: "1",
-    content: "This list is buggy"
-  },
-  {
-    id: "2",
-    content: "I hate lists like this..."
-  },
-  {
-    id: "3",
-    content: "Why would react do this to me"
-  },
-  {
-    id: "4",
-    content: "HAMBURGER PLEAAAAAAAAAAAAAAAASE"
-  },
+function arrangeIssue(provided: DraggableProvided, issue: Issue) {
+    return (
+        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <div style={issueStyle}>
+                <span>{issue.content}</span>
+            </div>
+        </div>
+    );
+}
+
+export const data: Issue[] = [
+    {
+        id: '1',
+        content: 'This list is buggy',
+    },
+    {
+        id: '2',
+        content: 'I hate lists like this...',
+    },
+    {
+        id: '3',
+        content: 'Why would react do this to me',
+    },
+    {
+        id: '4',
+        content: 'HAMBURGER PLEAAAAAAAAAAAAAAAASE',
+    },
 ];
 
-
+export const data2: Issue[] = [
+    {
+        id: '5',
+        content: 'This list is buggy',
+    },
+];
 
 export default App;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
