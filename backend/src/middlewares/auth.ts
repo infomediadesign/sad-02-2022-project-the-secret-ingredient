09f64219108edc6b4048e0737cc0a756bf4378d0
Niklas Lübcke <niklas.luebcke@innoplexia.com>
2022-06-08 14:26:13 +0200

    Test commit to check persistance of JWK.

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -22,18 +22,7 @@ export const authMiddleware = async (ctx: Context, next: Next) => {
     const data = await verify(jwt, key);
 
     if (data) {
-        // async function selectUser<T>(user: Model<T>, data: any) {
-        //     (await user.schema.findOne({ username: data.iss })) as any;
-        // }
-        // const model = User(db);
-        // const selectedUser = await selectUser(model, data);
-        // ctx.state.user = selectedUser;
         await next();
-        // const username = data.iss;
-        // ctx.state.user = username;
-
-        // ctx.state.user = username;
-        // console.log(username);
     } else {
         ctx.response.status = 401;
         return;
