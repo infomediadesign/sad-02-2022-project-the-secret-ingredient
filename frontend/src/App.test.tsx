3baf3ff07d9877d880bcaf6457ff4b025ed44aa1
Ani <aniruddh1297@gmail.com>
2022-06-09 23:34:01 +0200

    App tests modified

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,10 +2,3 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './Views/Board';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-//test
